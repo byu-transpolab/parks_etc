1ab8b80b3de7a90024667c40de6a35faf5390f36
connorwill <connor.williams@live.com>
2020-03-19 17:05:23 -0600

    this code creates a matrix of distances between all block IDs and all parks in NY city

--- new_york_origin_to_park_code.py
+++ new_york_origin_to_park_code.py
@@ -13,7 +13,7 @@ c['COUNTYFP'] = c['COUNTYFP'].apply('{0:0>3}'.format)
 c['TRACTCE'] = c['TRACTCE'].apply('{0:0>6}'.format)
 length = c.shape[0]
 county081 = c.query('COUNTYFP == "081"').index
-print(county081[1745])
+#print(county081[1745])
 county047 = c.query('COUNTYFP == "047"').index
 county061 = c.query('COUNTYFP == "061"').index
 county005 = c.query('COUNTYFP == "005"').index
@@ -58,7 +58,7 @@ c.drop(rows.index, inplace=True)
 
 frames = [hh, jj, kk, mm, nn]
 result = pd.concat(frames)
-print(result)
+#print(result)
 
 
 result = result.astype(str)
@@ -74,57 +74,41 @@ result['LONGITUDE'] = result['LONGITUDE'].astype(float)
 
 
 
-print(result)
-print('\n')
-print(result.shape[0])
-
-
-'''
-##block time
-############################################################
-xstart = result.iat[500,1]
-ystart = result.iat[500,2]
-B = ox.graph_from_place('New York, New York, USA', network_type='drive')
-#B = ox.graph_from_point((xstart, ystart), distance=2000, network_type='drive')
-origin = ox.get_nearest_node(B, (xstart, ystart))
-xend = 40.731456
-yend = -73.770367
-destination = ox.get_nearest_node(B, (xend, yend))
-
-bbox = ox.bbox_from_point((xend, yend), distance=500, project_utm=True)
-#this lat/long needs to be the same value as map "B" notated above
-B_proj = ox.project_graph(B)
-route = nx.shortest_path(B_proj, source=origin, target=destination, weight='length')
-fig, ax = ox.plot_graph_route(B_proj, route, bbox=bbox, node_size=0)
-#fig, ax = ox.plot_graph(ox.project_graph(B))
-
-route_lengths = ox.get_route_edge_attributes(B_proj, route, 'length')
-print('Total trip distance: {:,.0f} meters'.format(np.sum(route_lengths)))
-gg = np.sum(route_lengths)
-print(gg)
-'''
-B = ox.graph_from_place('New York, New York, USA', network_type='drive')
-B_proj = ox.project_graph(B)
-
-
+#print(result)
+#print('\n')
+#print(result.shape[0])
 print('__________________________________________________________\n')
-import pandas as pd
-u = pd.read_csv('tester.csv')
-print(u)
-u['lat'] = u['lat'].astype(float)
-u['long'] = u['long'].astype(float)
+
+u = pd.read_csv('the_data.csv')
+#print(u)
+u['latitude'] = u['latitude'].astype(float)
+u['longitude'] = u['longitude'].astype(float)
 
 
 col_names = []
-
-for name in range(0,5):
+qqq = result.shape[0]
+for name in range(0,qqq):
     b = result.iat[name,0]
     col_names.append(b)
 
+#print('length of result dataframe:')
+#print(result.shape[0])
+
+
+#x = 5
+rows_in_park_IDs = u.shape[0]
+park_IDs_orig = []
+park_IDs_orig.append(u.iat[0,0])
+#print(park_IDs_orig)
+count = 0
+for IDs in range(0,rows_in_park_IDs):
+    if (park_IDs_orig[count] != u.iat[IDs,0]):
+        count = count + 1
+        park_IDs_orig.append(u.iat[IDs,0])
+
 rows = []
-#rows.append(u.iat[0,0])
-for parkid in range(0,3):
-    f = u.iat[parkid,0]
+for parkid in range(0,len(park_IDs_orig)):
+    f = park_IDs_orig[parkid]
     rows.append(f)
 
 print('\n')
@@ -135,21 +119,51 @@ treasure = pd.DataFrame(index=rows,columns=col_names)
 total_rows = treasure.shape[0]
 #total_rows = total_rows-1
 total_columns = len(treasure.columns)
+#print(treasure)
 
 for tt in range(0,total_columns):
-    #treasure.iat[tt,0] = 5
-    for ii in range(0,total_rows):
-        xstart = result.iat[tt,1]
-        ystart = result.iat[tt,2]
-        origin = ox.get_nearest_node(B, (xstart, ystart))
-        xend = u.iat[ii,1]
-        yend = u.iat[ii,2]
-        destination = ox.get_nearest_node(B, (xend, yend))
-        route = nx.shortest_path(B_proj, source=origin, target=destination, weight='length')
-        route_lengths = ox.get_route_edge_attributes(B_proj, route, 'length')
-        gg = np.sum(route_lengths)
-        #print(gg)
-        treasure.iat[ii,tt] = gg
+    xstart = result.iat[tt,1]
+    ystart = result.iat[tt,2]
+    origin = ox.get_nearest_node(B, (xstart, ystart))
+    xend = u.iat[0,1]
+    yend = u.iat[0,2]
+    destination = ox.get_nearest_node(B, (xend, yend))
+    route = nx.shortest_path(B_proj, source=origin, target=destination, weight='length')
+    route_lengths = ox.get_route_edge_attributes(B_proj, route, 'length')
+    gg = np.sum(route_lengths)
+    treasure.iat[0,tt] = gg
 
 
-print(treasure)
+counter = 0
+rowz = rows_in_park_IDs - 1
+
+
+for jiber in range(1,rowz):
+    if (u.iat[jiber,0] == u.iat[jiber-1,0]):
+        for tt in range(0,total_columns):
+            xstart = result.iat[tt,1]
+            ystart = result.iat[tt,2]
+            origin = ox.get_nearest_node(B, (xstart, ystart))
+            xend = u.iat[jiber,1]
+            yend = u.iat[jiber,2]
+            destination = ox.get_nearest_node(B, (xend, yend))
+            route = nx.shortest_path(B_proj, source=origin, target=destination, weight='length')
+            route_lengths = ox.get_route_edge_attributes(B_proj, route, 'length')
+            aa = np.sum(route_lengths)
+            if (aa < treasure.iat[counter,tt]):
+                treasure.iat[counter,tt] = aa
+    else:
+        counter = counter + 1
+        for tt in range(0,total_columns):
+            xstart = result.iat[tt,1]
+            ystart = result.iat[tt,2]
+            origin = ox.get_nearest_node(B, (xstart, ystart))
+            xend = u.iat[jiber,1]
+            yend = u.iat[jiber,2]
+            destination = ox.get_nearest_node(B, (xend, yend))
+            route = nx.shortest_path(B_proj, source=origin, target=destination, weight='length')
+            route_lengths = ox.get_route_edge_attributes(B_proj, route, 'length')
+            aa = np.sum(route_lengths)
+            treasure.iat[counter,tt] = aa
+
+#print(treasure)
